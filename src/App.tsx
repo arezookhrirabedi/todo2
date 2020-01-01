11f1c58c55340a9a6ff6574c84d0155acdb09ff5
arezooKheirabadi <a_kheirabadi@yahoo.com>
2020-01-01 12:47:13 +0330

    li list

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import StudentRate from './StudentRate';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      branch dev created
-    </div>
-  );
+class App extends React.Component {
+  studentList: {
+    name: string;
+    rate: number;
+    age: number;
+  }[] = [
+      { name: "taghi", rate: 17, age: 32 },
+      { name: "mahdi", rate: 2, age: 40 }
+    ];
+
+  detail(item: { name: string; rate: number; age: number }, index: number) {
+    return <li key={index} className="kk">{item.name + " " + item.age + ' ' + item.rate}</li>
+  }
+
+
+  render() {
+    return (
+      // <StudentRate />
+      <ul className="wrapper">
+        {this.studentList.map(this.detail)}
+      </ul>
+    )
+  }
 }
-
 export default App;
---
+++ src/StudentRate.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class StudentRate extends React.Component {
+
+    render() {
+        return (
+            <>
+                <h1>salam</h1>
+            </>
+        );
+    }
+}
+
+
+export default StudentRate;
